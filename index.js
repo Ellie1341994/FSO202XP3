4a62711e7558af738071578c179d510f19a16b17
Ellie1341994 <eli1341994@gmail.com>
2023-03-28 10:11:13 -0300

    Add Ex 3.9 cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const logger = require('./logger')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(logger)
 let phonebookEntries = [
