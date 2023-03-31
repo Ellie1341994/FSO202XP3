aa7c8c14e4877f4a3a07ed76238eba4bcb74c359
Ellie1341994 <eli1341994@gmail.com>
2023-03-30 21:36:52 -0300

    Add ESLint customized config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+    "indent": [
+      "error",
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  },
+};
